c26dd7a9c5cee9349ab979c39e137f5567168dc4
Thomas Cherryhomes <thom.cherryhomes@gmail.com>
2025-11-15 09:18:41 -0600

    [msdos] start implementing graphics.

--- src/msdos/graphics.c
+++ src/msdos/graphics.c
@@ -2,12 +2,26 @@
   Graphics functionality
 */
 
+#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
+#include <dos.h>
+#include <i86.h>
 #include <string.h>
 #include "../platform-specific/graphics.h"
 #include "../platform-specific/sound.h"
 #include "../misc.h"
+#include "charset.h"
 
+#define VIDEO_RAM_ADDR ((unsigned char far *)0xB8000000UL)
+#define VIDEO_LINE_BYTES 80
+#define VIDEO_ODD_OFFSET 0x2000
+
+uint8_t far *video = VIDEO_RAM_ADDR;
 extern uint8_t charset[];
+int oldmode=0;
+uint8_t mask=0;
+
 #define OFFSET_Y 2
 
 #define ROP_CPY 0xff
@@ -41,20 +55,97 @@ uint16_t quadrant_offset[] = {
 
 uint16_t legendShipOffset[] = {2, 1, 0, 256U * 5, 256U * 6 + 1};
 
+/**
+ * @brief plot a 8x8 2bpp tile to screen at column x, row y
+ * @param tile ptr to 2bpp tile data * 8
+ * @param x Column 0-39
+ * @param y Row 0-24
+ */
+void plot_tile(const unsigned char *tile, unsigned char x, unsigned char y)
+{
+    unsigned char i=0;
+
+    if (y<25)
+        y <<= 3; // Convert row to line
+
+    x <<= 1; // Convert column to video ram offset
+
+    for (i=0;i<8;i++)
+    {
+        unsigned char r = y + i;
+        unsigned char rh = r >> 1; // Because CGA is interleaved odd/even.
+        unsigned short ro = rh * VIDEO_LINE_BYTES + x;
+
+        // If row is odd, go into second bank.
+        if (r & 1)
+            ro += VIDEO_ODD_OFFSET;
+
+        // Put tile data into video RAM.
+        video[ro] = tile[i*2];
+        video[ro+1] = tile[i*2+1];
+    }
+}
+
+/**
+ * @brief Clear screen to given color index
+ * @param c Color to set in all spots
+ */
+void clear(unsigned char c)
+{
+    unsigned char b = ((c & 0x03) | ((c & 0x03) << 2) | ((c & 0x03) << 4) | ((c & 0x03) << 6));
+
+    _fmemset(&video[0x0000], b, 8000-640);
+    _fmemset(&video[0x2000], b, 8000-640);
+}
+
+
 void updateColors()
 {
 }
 
 uint8_t cycleNextColor()
 {
-}
-
-void rgbOrComposite()
-{
+    return 0;
 }
 
 void initGraphics()
 {
+    union REGS r;
+    uint8_t *c;
+    uint16_t i;
+
+    // Get old mode
+    r.h.ah = 0x0f;
+    int86(0x10,&r,&r);
+
+    oldmode=r.h.al;
+
+    // Set graphics mode
+    r.h.ah = 0x00;
+    r.h.al = 0x04; // 320x200x4
+    int86(0x10,&r,&r);
+
+    // Remap palette colors, if possible
+    r.h.ah = 0x10;
+    r.h.al = 0x00;
+    r.h.bl = 1;             // Color 1
+    r.h.bh = 10;            // to light green.
+    int86(0x10,&r,&r);
+
+    r.h.ah = 0x10;
+    r.h.al = 0x00;
+    r.h.bl = 2;             // Color 2
+    r.h.bh = 4;             // To dark red
+    int86(0x10,&r,&r);
+
+    // Create a second set of white on black text for status bar
+    // It could also be achieved via a mask in exchange for more memory
+    c=&ascii[0][0];
+    for(i=0;i<sizeof(ascii)/2;i++) {
+        // Set  ascii characters to white on black
+        *(c+1536)= (*c^0x55)*3;
+        c++;
+    }
 }
 
 bool saveScreenBuffer()
@@ -73,6 +164,22 @@ void drawPlayerName(uint8_t player, const char *name, bool active)
 
 void drawText(uint8_t x, uint8_t y, const char *s)
 {
+    signed char c=0;
+
+    while (c = *s++)
+    {
+        c -= 32;
+        if (c<0 || c>96)
+            continue;
+
+        if (x>39)
+        {
+            x=0;
+            y++;
+        }
+
+        plot_tile(&ascii[c+mask],x++,y);
+    }
 }
 
 void drawTextAt(uint8_t x, uint8_t y, const char *s)
